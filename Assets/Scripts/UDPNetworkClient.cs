80a7ae5848745a40e8fad3eace8abec1d22c7aac
Dilmer Valecillos <dvalecillos@stampinup.com>
2021-08-23 17:53:10 -0600

    Added additional code to udp client

--- Assets/Scripts/UDPNetworkClient.cs
+++ Assets/Scripts/UDPNetworkClient.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using UnityEngine;
 
 public class UDPNetworkClient : MonoBehaviour
 {
+    private static readonly object lockObject = new object();
+
     [SerializeField]
     private string hostOrIPAddress;
 
@@ -16,29 +19,66 @@ public class UDPNetworkClient : MonoBehaviour
     [SerializeField]
     private string rawCommand;
 
+    [SerializeField]
+    private string returnData;
+
     private UdpClient UdpClient { set; get; }
+
+
+    private Queue<string> commands = new Queue<string>();
 
     private void Awake()
     {
         UdpClient = new UdpClient();
-        InitializeUDPConnection();
+        ThreadStart threadStart = new ThreadStart(InitializeUDPConnection);
+        Thread thread = new Thread(threadStart);
+        thread.Start();
+    }
+
+    private void Update()
+    {
+        if(commands.Count > 0)
+        {
+            lock (lockObject)
+            {
+                Debug.Log("Received: " + returnData);
+            }
+        }
+    }
+
+    public void SendCommand()
+    {
+        commands.Enqueue(rawCommand);
     }
 
     public void InitializeUDPConnection()
     {
         UdpClient.Connect(hostOrIPAddress, port);
 
-        byte[] message = Encoding.ASCII.GetBytes(rawCommand);
+        while (true)
+        {
+            //IPEndPoint object will allow us to read datagrams sent from any source.
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
 
-        UdpClient.Send(message, message.Length);
+            // Blocks until a message returns on this socket from a remote host.
+            byte[] receiveBytes = UdpClient.Receive(ref RemoteIpEndPoint);
 
-        //IPEndPoint object will allow us to read datagrams sent from any source.
-        IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            lock (lockObject)
+            {
+                // process commands
+                if (commands.Count > 0)
+                {
+                    byte[] message = Encoding.ASCII.GetBytes(rawCommand);
+                    UdpClient.Send(message, message.Length);
+                }
 
-        // Blocks until a message returns on this socket from a remote host.
-        byte[] receiveBytes = UdpClient.Receive(ref RemoteIpEndPoint);
-
-        string returnData = Encoding.ASCII.GetString(receiveBytes);
+                returnData = Encoding.ASCII.GetString(receiveBytes);
+                if (!string.IsNullOrEmpty(returnData))
+                {
+                    processData = true;
+                }
+            }
+        }
     }
 
     private void OnDestroy()
