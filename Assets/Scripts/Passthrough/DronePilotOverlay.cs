5262f7867afabf886766c275f7c3d6e7450774a4
Dilmer Valecillos <dilmervst@gmail.com>
2021-08-24 22:46:22 -0600

    Added drone pilot passthrough overlay

---
+++ Assets/Scripts/Passthrough/DronePilotOverlay.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+public class DronePilotOverlay : MonoBehaviour
+{
+    [SerializeField]
+    private OVRPassthroughLayer ovrPassthroughLayer;
+
+    private bool invalid = false;
+
+    private void Awake()
+    {
+        if (ovrPassthroughLayer == null)
+        {
+            Logger.Instance.LogError("Drone Pilot is missing serialize fields");
+            invalid = true;
+        }
+
+        if (invalid) return;
+
+        ovrPassthroughLayer.AddSurfaceGeometry(gameObject, true);
+    }
+
+    private void OnDestroy()
+    {
+        ovrPassthroughLayer.RemoveSurfaceGeometry(gameObject);
+    }
+}
